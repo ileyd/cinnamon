105dd41a8d44dc50d69234c9bf5e082bc4a63493
Luna <luna@stor.lol>
2017-08-19 17:10:23 +1000

    integrated streaming functionality into TorrentList.vue

--- src/renderer/components/TorrentList.vue
+++ src/renderer/components/TorrentList.vue
@@ -10,8 +10,8 @@
                 </v-toolbar>
                 <div class="text-xs-center" v-if="online">
                 <v-progress-circular
-                        v-bind:indeterminate="!online"
-                        v-bind:active="!online"
+                        indeterminate
+                        v-bind:active="!online || loading"
                         class="primary--text"
                 ></v-progress-circular>
                 </div>
@@ -51,7 +51,7 @@
                         </v-list-tile-action>
                         <v-list-tile-action>
                             <v-spacer></v-spacer>
-                                <v-btn icon ripple dark slot="activator" @click.native="setMagnetURI(item.magnet)" to="player">
+                                <v-btn icon ripple dark slot="activator" @click.native="stream(item.magnet)">
                                     <v-icon class="purple--text text--lighten-2">play_circle_filled</v-icon>
                                 </v-btn>
                             <v-spacer></v-spacer>
@@ -66,19 +66,25 @@
 <script>
 /* eslint-disable indent */
 import pantsu from 'pantsu-api'
+import WebTorrent from 'webtorrent'
+import MPV from 'node-mpv'
 import { mapGetters } from 'vuex'
 export default {
   name: 'TorrentList',
   data () {
     return {
         torrents: [],
-        snackbar: false
+        snackbar: false,
+        loading: false,
+        mpv: new MPV(),
+        videoServer: null
     }
   },
   computed: mapGetters([
     'query',
     'limit',
-    'online'
+    'online',
+    'filePath'
   ]),
   mounted () {
       pantsu.search(this.query, this.limit).then((data) => {
@@ -90,9 +96,25 @@ export default {
       })
   },
   methods: {
-      setMagnetURI: function (data) {
-          this.$store.commit('setMagnetURI', data)
-      }
+    setMagnetURI: function (data) {
+        this.$store.commit('setMagnetURI', data)
+    },
+    stream: function (magnetURI) {
+      this.loading = true
+      console.log('instructed to stream', magnetURI)
+      let wt = new WebTorrent()
+      let self = this
+      wt.add(magnetURI, function (torrent) {
+        self.videoServer = torrent.createServer()
+        self.videoServer.listen(0)
+        console.log('video server listening on http://localhost:' + self.videoServer.address().port)
+        let file = torrent.files.find(function (file) {
+          return file.name.endsWith('.mp4') || file.name.endsWith('.mkv') || file.name.endsWith('.avi')
+        })
+        self.$store.commit('setFilePath', file.path)
+        self.mpv.loadStream('http://localhost' + ':' + self.videoServer.address().port + '/0/' + self.filePath)
+      })
+    }
   }
 }
 </script>
